1b34600fce1e4fdbb9e65a6e02ef7443bdd21ed3
81827cr <81827cr@gmail.com>
2024-07-20 09:39:25 +0800

    laster

--- functions/upload.js
+++ functions/upload.js
@@ -1,22 +1,21 @@
-// export async function onRequestPost(context) {
-//   // Contents of context object
-//   const {
-//     request, // same as existing Worker API
-//     env, // same as existing Worker API
-//     params, // if filename includes [id] or [[path]]
-//     waitUntil, // same as ctx.waitUntil in existing Worker API
-//     next, // used for middleware or to fetch assets
-//     data, // arbitrary space for passing data between middlewares
-//   } = context;
-//   context.request;
-//   const url = new URL(request.url);
-//   console.log(' -----------------------')
-//   console.log('onRequestPost upload url:', url)
-//   console.log(' -----------------------')
-//   const response = fetch("https://telegra.ph/" + url.pathname + url.search, {
-//     method: request.method,
-//     headers: request.headers,
-//     body: request.body,
-//   });
-//   return response;
-// }
+import { errorHandling, telemetryData } from "./utils/middleware";
+export async function onRequestPost(context) {  // Contents of context object  
+    const {
+        request, // same as existing Worker API    
+        env, // same as existing Worker API    
+        params, // if filename includes [id] or [[path]]   
+        waitUntil, // same as ctx.waitUntil in existing Worker API    
+        next, // used for middleware or to fetch assets    
+        data, // arbitrary space for passing data between middlewares 
+    } = context;
+    const clonedRequest = request.clone();
+    await errorHandling(context);
+    telemetryData(context);
+    const url = new URL(clonedRequest.url);
+    const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
+        method: clonedRequest.method,
+        headers: clonedRequest.headers,
+        body: clonedRequest.body,
+    });
+    return response;
+}
